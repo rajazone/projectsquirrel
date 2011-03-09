1ce2d646e600a9e810722d6a5b5aaabb12e9e5ae
allie516@gmail.com <allie516@gmail.com@2f7d6494-a244-5127-6f32-e4e80e1f47cf>
2011-03-09 07:59:34 +0000

    First upload of project. Includes 2 Activities [main & observ].

---
+++ trunk/src/edu/uic/cs/projectsquirrel/main.java
@@ -0,0 +1,39 @@
+/* Main/Startup Page
+ * -----------------
+ * Displays Title & Navigation Buttons
+ * 
+ */
+
+package edu.uic.cs.projectsquirrel;
+
+import java.io.IOException;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import android.view.View;
+import android.widget.Button;
+
+public class main extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        Button newObs = (Button) findViewById(R.id.button1);
+        Button sqGuide = (Button) findViewById(R.id.button2);
+        
+        //Set listener for "New Observation" button on main screen.
+        newObs.setOnClickListener(new View.OnClickListener(){	
+	    	public void onClick(View v){
+	    		Intent i = new Intent(getApplicationContext(), observ.class);
+	            startActivity(i);
+	    		
+	    		
+	    }});
+        
+        
+    }//end onCreate
+}//end main
