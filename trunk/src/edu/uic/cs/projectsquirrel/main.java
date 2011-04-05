bb36619f9c34a7b3594be1cf633595f6e3f55bd4
rrogow2@gmail.com <rrogow2@gmail.com@2f7d6494-a244-5127-6f32-e4e80e1f47cf>
2011-04-05 03:15:40 +0000

    Added date picker and time picker

--- trunk/src/edu/uic/cs/projectsquirrel/main.java
+++ trunk/src/edu/uic/cs/projectsquirrel/main.java
@@ -14,6 +14,7 @@ import android.os.Bundle;
 
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class main extends Activity {
     /** Called when the activity is first created. */
@@ -21,6 +22,7 @@ public class main extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
         
         Button newObs = (Button) findViewById(R.id.button1);
         Button sqGuide = (Button) findViewById(R.id.button2);
