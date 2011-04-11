517cc4567b784b2175742fdbc8892b7a6c39bd68
rrogow2@gmail.com <rrogow2@gmail.com@2f7d6494-a244-5127-6f32-e4e80e1f47cf>
2011-04-11 03:15:11 +0000

    Observ Class is finished. Someone can check to make sure. If user manually enters zip code then we do not get lat and long. not sure if we need to do that.

--- trunk/src/edu/uic/cs/projectsquirrel/observ.java
+++ trunk/src/edu/uic/cs/projectsquirrel/observ.java
@@ -9,7 +9,9 @@ package edu.uic.cs.projectsquirrel;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -20,6 +22,7 @@ import android.os.Bundle;
 import android.provider.Settings;
 import android.graphics.Shader;
 
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
@@ -27,20 +30,39 @@ import java.text.DateFormat;
 import java.io.IOException;
 import java.lang.String;
 
+import android.telephony.TelephonyManager;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.TimePicker;
 
 public class observ extends Activity {
 	
 	Bundle BUNDL;	//Used to transfer Log data between activities.
-	
+	private int mYear;
+    private int mMonth;
+    private int mDay;
+    private String Day;
+    private String Month;
+    private String Year;
+    
+    private int mHour;
+    private int mMinute;
+    private String Hour;
+    private String Minute;
+    private String ampm;
+
+    static final int CHECK_EXIT_DIALOG_ID = 0;
+    static final int TIME_DIALOG_ID = 2;
+    static final int NO_ZIP_DIALOG_ID = 3;
+    static final int DATE_DIALOG_ID = 1;
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -54,12 +76,15 @@ public class observ extends Activity {
         LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
 
         
-        //
+
+		//
         //
         //
         if ( !locationManager.isProviderEnabled( LocationManager.GPS_PROVIDER ) ) {
             buildAlertMessageNoGps();
         }
+        else
+        {
 
 
         //
@@ -111,15 +136,18 @@ public class observ extends Activity {
             
             };
             
-            //TODO: (Robin?) If GPS is disabled & user is about to submit data...
+            //TODO: (Robin) If GPS is disabled & user is about to submit data...
             //- Grab zip code from  (EditText) findViewById(R.id.editText1);
+            // CHECK
             //- If zip code does not exist, inform user
+            //	CHECK
             //- Else convert zip code into approx. latitude & logitude
             //- Store zip code, lat. & long. as Strings into bundl with key names LATITUDE, LOGITUDE, ZIP -> BUNDL.putString("key", "value");
+            // CHECK
 
         // Register the listener with the Location Manager to receive location updates
         locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
-        
+        }
         
        // *
        // *
@@ -187,15 +215,31 @@ public class observ extends Activity {
         	public void onClick(View v){
         		//TODO: Create widget DatePicker in a dialog to be displayed at this point.
         		//BUNDL.Day,Month,Year = this new entry
+        		showDialog(DATE_DIALOG_ID);
+        		
 	    }});
+     // get the current date
+        final Calendar c = Calendar.getInstance();
+        mYear = c.get(Calendar.YEAR);
+        mMonth = c.get(Calendar.MONTH);
+        mDay = c.get(Calendar.DAY_OF_MONTH);
+
+        // display the current date (this method is below)
+        updateDisplay();
         
+
         //Allow users to edit time
         timeBox.setOnClickListener(new View.OnClickListener(){	
         	public void onClick(View v){
         		//TODO: Create widget TimePicker in a dialog to be displayed at this point.
         		//BUNDL.Hour,Minute,AMPM = this new entry
+        		showDialog(TIME_DIALOG_ID);
 	    }});
         
+        mHour = c.get(Calendar.HOUR_OF_DAY);
+        mMinute = c.get(Calendar.MINUTE);
+        
+        updateDisplay();
         
         //NEXT BUTTON
         next.setOnClickListener(new View.OnClickListener(){	
@@ -206,9 +250,19 @@ public class observ extends Activity {
 		            BUNDL.putString("NUM_FOX_SQUIRRELS",fox_text.getText().toString());
 		            BUNDL.putString("NUM_GRAY_SQUIRRELS",gray_text.getText().toString());
 		            
-	    		Intent i = new Intent(getApplicationContext(), animals.class);
-	    		i.putExtras(BUNDL);	//Sends BUNDL to next activity.
-	            startActivity(i);
+		            EditText txt = (EditText) findViewById(R.id.editText1);
+		            String str = txt.getText().toString();
+		            if(str.equals(""))
+		            {
+		            	showDialog(NO_ZIP_DIALOG_ID);
+		            }
+		            else
+		            {
+		            	BUNDL.putString("ZIP", txt.getText().toString());
+		            	Intent i = new Intent(getApplicationContext(), animals.class);
+		            	i.putExtras(BUNDL);	//Sends BUNDL to next activity.
+		            	startActivity(i);
+		            }
 	    }});
         
         //CANCEL BUTTON  
@@ -297,22 +351,110 @@ public class observ extends Activity {
         // End Squirrel Counters -------------------------------------------------------------------
         
     }// End onCreate
+   
     
-    @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event)  {
-        if (keyCode == KeyEvent.KEYCODE_BACK && event.getRepeatCount() == 0) {
-        	showDialog(0);
-            return true;
-        }
-        return super.onKeyDown(keyCode, event);
-    }
+
     
-    protected Dialog onCreateDialog(int i) 
+    private void updateDisplay() {
+		// TODO Auto-generated method stub
+    	
+    	//Update the Date when changed
+    	Button dateBox = (Button) findViewById(R.id.date2);	//retrieve date button
+    	Button timeBox = (Button) findViewById(R.id.time2); //retrieve time button
+                
+                        // Month is 0 based so add 1
+    					if(String.valueOf(mMonth).length()==2)
+    						Month = String.valueOf((mMonth + 1));
+    					else
+    						Month = "0"+String.valueOf((mMonth + 1));
+                        if(String.valueOf(mDay).length()==1)
+                        	Day = ("0"+String.valueOf(mDay)+"/");
+                        else
+                        Day = String.valueOf(mDay)+("/");
+                        Year = String.valueOf(mYear).substring(2);
+                        
+
+                        dateBox.setText(Month+"/"+Day+"/"+Year);
+                        BUNDL.putString("Day", Day);
+                        BUNDL.putString("Month", Month);
+                        BUNDL.putString("Year", Year);
+                        
+        //Update the Time when it is changed
+                        if(mHour<12)
+                        {
+                        	if(mHour<10)
+                        	{
+                        	Hour = "0"+String.valueOf(mHour);
+                        	}
+                        	else
+                        	{
+                        		Hour = String.valueOf(mHour);
+                        	}
+                        	if((mHour)==0)
+                        	{
+                        		Hour = "12";
+                        	}
+                        		ampm = "AM";
+                        }
+                        else
+                        {
+                        	Hour = String.valueOf(mHour-12);
+                        	if(Hour.length()==1)
+                        	{
+                        		Hour = "0"+Hour;
+                        	}
+                        	if((mHour-12)==0)
+                        	{
+                        		Hour = "12";
+                        	}
+                        	ampm = "PM";
+                        }
+                        if(mMinute<10)
+                        {
+                        	Minute = "0"+String.valueOf(mMinute);
+                        }
+                        else
+                        {
+                        	Minute = String.valueOf(mMinute);
+                        }
+                        timeBox.setText(Hour+":"+Minute+ " " + ampm);
+                        
+                        BUNDL.putString("Hour", Hour);
+                        BUNDL.putString("Minute", Minute);
+                        BUNDL.putString("AMPM", ampm);
+	}
+ // the callback received when the user "sets" the time in the dialog
+    private TimePickerDialog.OnTimeSetListener mTimeSetListener =
+        new TimePickerDialog.OnTimeSetListener() {
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                mHour = hourOfDay;
+                mMinute = minute;
+                updateDisplay();
+            }
+        };
+ // the callback received when the user "sets" the date in the dialog
+    private DatePickerDialog.OnDateSetListener mDateSetListener =
+            new DatePickerDialog.OnDateSetListener() {
+
+                public void onDateSet(DatePicker view, int year, 
+                                      int monthOfYear, int dayOfMonth) {
+                    mYear = year;
+                    mMonth = monthOfYear;
+                    mDay = dayOfMonth;
+                    updateDisplay();
+                }
+            };
+
+
+
+	protected Dialog onCreateDialog(int i) 
 	{
     	Dialog dialog = null;
+    	AlertDialog alert;
+    	AlertDialog.Builder builder;
 		switch(i) {
 		case 0:
-			AlertDialog.Builder builder = new AlertDialog.Builder(this);
+			builder = new AlertDialog.Builder(this);
 			builder.setMessage("Are you sure you wish to exit? Data will be lost.")
 			       .setCancelable(false)
 			       .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
@@ -325,8 +467,27 @@ public class observ extends Activity {
 			                dialog.cancel();
 			           }
 			       });
-			AlertDialog alert = builder.create();
+			alert = builder.create();
 			return alert;
+		case DATE_DIALOG_ID:
+	        return new DatePickerDialog(this,
+	                    mDateSetListener,
+	                    mYear, mMonth, mDay);
+		case TIME_DIALOG_ID:
+	        return new TimePickerDialog(this,
+	                mTimeSetListener, mHour, mMinute, false);
+		case NO_ZIP_DIALOG_ID:
+		    builder = new AlertDialog.Builder(this);
+			builder.setMessage("You need to enter a zip code.")
+			       .setCancelable(false)
+			       .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+			           public void onClick(DialogInterface dialog, int id) {
+			        	   dialog.cancel();
+			           }
+			       });
+			 alert = builder.create();
+			 return alert;
+	    
 		}
 		
 		return dialog;
@@ -340,6 +501,7 @@ public class observ extends Activity {
                        //launchGPSOptions(); 
                 	   Intent myIntent = new Intent( Settings.ACTION_SECURITY_SETTINGS );
                 	    startActivity(myIntent);
+                	    
                    }
                })
                .setNegativeButton("No", new DialogInterface.OnClickListener() {
@@ -349,6 +511,76 @@ public class observ extends Activity {
                });
         final AlertDialog alert = builder.create();
         alert.show();
+        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        if(locationManager.isProviderEnabled( LocationManager.GPS_PROVIDER))
+        {
+
+
+        //
+        //
+        //
+        // Define a listener that responds to location updates
+        LocationListener locationListener = new LocationListener() {
+            public void onLocationChanged(Location loc){
+            //updateWithNewLocation(loc);
+            Geocoder gcd = new Geocoder(getApplicationContext(), Locale.getDefault());
+            List<Address> addresses = null;
+			try {
+				addresses = gcd.getFromLocation(loc.getLatitude(), loc.getLongitude(), 1);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+            if (addresses.size() > 0){
+                String Text = addresses.get(0).getLocality();
+                EditText txt = (EditText) findViewById(R.id.editText1);
+                txt.setText(addresses.get(0).getPostalCode()); 
+                
+                //TODO: Need latitude and logitude
+                //Save them...
+                BUNDL.putString("LATITUDE", String.valueOf(loc.getLatitude()));
+                BUNDL.putString("LONGITUDE", String.valueOf(loc.getLongitude()));
+                BUNDL.putString("ZIP", String.valueOf(addresses.get(0).getPostalCode()));
+            }
+            }
+
+			//@Override
+			public void onProviderDisabled(String provider) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			//@Override
+			public void onProviderEnabled(String provider) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			//@Override
+			public void onStatusChanged(String provider, int status,
+					Bundle extras) {
+				// TODO Auto-generated method stub
+				
+			}
+            
+            };
+            
+            //TODO: (Robin?) If GPS is disabled & user is about to submit data...
+            //- Grab zip code from  (EditText) findViewById(R.id.editText1);
+            //- If zip code does not exist, inform user
+            //- Else convert zip code into approx. latitude & logitude
+            //- Store zip code, lat. & long. as Strings into bundl with key names LATITUDE, LOGITUDE, ZIP -> BUNDL.putString("key", "value");
+
+        // Register the listener with the Location Manager to receive location updates
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+        }
+    }
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK)) {
+            showDialog(0);
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
     }
    
     }/* End of UseGps Activity */
